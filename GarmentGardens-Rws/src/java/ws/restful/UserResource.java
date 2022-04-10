aad967e27b93f4d58a688244ea9c05f11c14390e
Wong Deshun <77624433+Deshun99@users.noreply.github.com>
2022-04-10 12:27:12 +0800

    Modified User Resources

--- GarmentGardens-Rws/src/java/ws/restful/UserResource.java
+++ GarmentGardens-Rws/src/java/ws/restful/UserResource.java
@@ -12,12 +12,17 @@ import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import util.exception.InputDataValidationException;
 import util.exception.InvalidLoginCredentialException;
+import util.exception.UnknownPersistenceException;
+import util.exception.UserUsernameExistException;
+import ws.datamodel.CreateUserReq;
 
 /**
  * REST Web Service
@@ -69,6 +74,42 @@ public class UserResource {
         catch(Exception ex)
         {
             return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+        }
+    }
+    
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createUser(CreateUserReq createUserReq)
+    {
+        if(createUserReq != null)
+        {
+            try
+            {
+                Long userEntity  = userEntitySessionBeanLocal.createNewUser(createUserReq.getUserEntity());                
+                
+                return Response.status(Response.Status.OK).entity(userEntity).build();
+            }
+            catch(UserUsernameExistException ex)
+            {
+                return Response.status(Status.UNAUTHORIZED).entity(ex.getMessage()).build();
+            }
+            catch(UnknownPersistenceException ex)
+            {
+                return Response.status(Response.Status.BAD_REQUEST).entity(ex.getMessage()).build();
+            }
+            catch(InputDataValidationException ex)
+            {
+                return Response.status(Response.Status.BAD_REQUEST).entity(ex.getMessage()).build();
+            }
+            catch(Exception ex)
+            {
+                return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+            }
+        }
+        else
+        {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Invalid create new user request").build();
         }
     }
 }
