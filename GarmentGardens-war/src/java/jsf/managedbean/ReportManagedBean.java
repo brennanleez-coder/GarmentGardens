22e108ef7d4a1ebb1806606a926497d5c278229e
brennan-lee <brennan.lee@u.nus.edu>
2022-04-17 16:48:53 +0800

    ALL JASPER REPORTS

--- GarmentGardens-war/src/java/jsf/managedbean/ReportManagedBean.java
+++ GarmentGardens-war/src/java/jsf/managedbean/ReportManagedBean.java
@@ -5,10 +5,15 @@
  */
 package jsf.managedbean;
 
+import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.time;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.HashMap;
 import javax.annotation.Resource;
 import javax.inject.Named;
@@ -39,7 +44,12 @@ public class ReportManagedBean {
     public void generateProductReport(ActionEvent event) {
         try {
             HashMap parameters = new HashMap();
-            //parameters.put("Description", "This is a placeholder description");
+            LocalDateTime myDateObj = LocalDateTime.now();
+            System.out.println("Before formatting: " + myDateObj);
+            DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+            String formattedDate = myDateObj.format(myFormatObj);
+            parameters.put("Description", "GENERATED ON " + formattedDate);
             InputStream reportStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/jasperreport/productReport.jasper");
             OutputStream outputStream = FacesContext.getCurrentInstance().getExternalContext().getResponseOutputStream();
             JasperRunManager.runReportToPdfStream(reportStream, outputStream, parameters, garmentGardensDataSource.getConnection());
@@ -49,18 +59,21 @@ public class ReportManagedBean {
             ex.printStackTrace();
         } catch (IOException ex) {
             ex.printStackTrace();
-        }
-        catch(Exception ex)
-        {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
-    
-        public void generateCategoryReport(ActionEvent event) {
+
+    public void generateCategoryReport(ActionEvent event) {
         try {
             HashMap parameters = new HashMap();
-            //parameters.put("Description", "This is a placeholder description");
-            InputStream reportStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/jasperreport/categoryreport.jasper");
+            LocalDateTime myDateObj = LocalDateTime.now();
+            System.out.println("Before formatting: " + myDateObj);
+            DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+            String formattedDate = myDateObj.format(myFormatObj);
+            parameters.put("Description", "GENERATED ON " + formattedDate);
+            InputStream reportStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/jasperreport/categoryReport.jasper");
             OutputStream outputStream = FacesContext.getCurrentInstance().getExternalContext().getResponseOutputStream();
             JasperRunManager.runReportToPdfStream(reportStream, outputStream, parameters, garmentGardensDataSource.getConnection());
         } catch (JRException ex) {
@@ -69,11 +82,123 @@ public class ReportManagedBean {
             ex.printStackTrace();
         } catch (IOException ex) {
             ex.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        catch(Exception ex)
-        {
+    }
+
+    public void generateTagReport(ActionEvent event) {
+        try {
+            HashMap parameters = new HashMap();
+            LocalDateTime myDateObj = LocalDateTime.now();
+            System.out.println("Before formatting: " + myDateObj);
+            DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+            String formattedDate = myDateObj.format(myFormatObj);
+            parameters.put("Description", "GENERATED ON " + formattedDate);
+            InputStream reportStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/jasperreport/tagReport.jasper");
+            OutputStream outputStream = FacesContext.getCurrentInstance().getExternalContext().getResponseOutputStream();
+            JasperRunManager.runReportToPdfStream(reportStream, outputStream, parameters, garmentGardensDataSource.getConnection());
+        } catch (JRException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void generateDisputeReport(ActionEvent event) {
+        try {
+            HashMap parameters = new HashMap();
+            LocalDateTime myDateObj = LocalDateTime.now();
+            System.out.println("Before formatting: " + myDateObj);
+            DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+            String formattedDate = myDateObj.format(myFormatObj);
+            parameters.put("Description", "GENERATED ON " + formattedDate);
+            InputStream reportStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/jasperreport/disputeReport.jasper");
+            OutputStream outputStream = FacesContext.getCurrentInstance().getExternalContext().getResponseOutputStream();
+            JasperRunManager.runReportToPdfStream(reportStream, outputStream, parameters, garmentGardensDataSource.getConnection());
+        } catch (JRException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void generateRewardReport(ActionEvent event) {
+        try {
+            HashMap parameters = new HashMap();
+            LocalDateTime myDateObj = LocalDateTime.now();
+            System.out.println("Before formatting: " + myDateObj);
+            DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+            String formattedDate = myDateObj.format(myFormatObj);
+            parameters.put("Description", "GENERATED ON " + formattedDate);
+            InputStream reportStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/jasperreport/rewardReport.jasper");
+            OutputStream outputStream = FacesContext.getCurrentInstance().getExternalContext().getResponseOutputStream();
+            JasperRunManager.runReportToPdfStream(reportStream, outputStream, parameters, garmentGardensDataSource.getConnection());
+        } catch (JRException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+        public void generateMotdReport(ActionEvent event) {
+        try {
+            HashMap parameters = new HashMap();
+            LocalDateTime myDateObj = LocalDateTime.now();
+            System.out.println("Before formatting: " + myDateObj);
+            DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+            String formattedDate = myDateObj.format(myFormatObj);
+            parameters.put("Description", "GENERATED ON " + formattedDate);
+            InputStream reportStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/jasperreport/motdReport.jasper");
+            OutputStream outputStream = FacesContext.getCurrentInstance().getExternalContext().getResponseOutputStream();
+            JasperRunManager.runReportToPdfStream(reportStream, outputStream, parameters, garmentGardensDataSource.getConnection());
+        } catch (JRException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
         
+            public void generateOrderReport(ActionEvent event) {
+        try {
+            HashMap parameters = new HashMap();
+            LocalDateTime myDateObj = LocalDateTime.now();
+            System.out.println("Before formatting: " + myDateObj);
+            DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+            String formattedDate = myDateObj.format(myFormatObj);
+            parameters.put("Description", "GENERATED ON " + formattedDate);
+            InputStream reportStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/jasperreport/orderReport.jasper");
+            OutputStream outputStream = FacesContext.getCurrentInstance().getExternalContext().getResponseOutputStream();
+            JasperRunManager.runReportToPdfStream(reportStream, outputStream, parameters, garmentGardensDataSource.getConnection());
+        } catch (JRException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
